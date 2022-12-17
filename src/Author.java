12e8bf495f8553852f7d66268aef5fc8b05b4cf4
IVIaike <trainer.ekb@gmail.com>
2022-12-17 22:57:39 +0500

    1st commit

---
+++ src/Author.java
@@ -0,0 +1,9 @@
+public class Author {
+    String authorFirstName;
+    String getAuthorLastName;
+
+    public Author (String authorFirstName, String getAuthorLastName) {
+        this.authorFirstName = authorFirstName;
+        this.getAuthorLastName = getAuthorLastName;
+    }
+}
---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    String bookTitle;
+    String authorName;
+    int publishingYear;
+
+    public Book (String bookTitle, String authorName, int publishingYear) {
+        this.bookTitle = bookTitle;
+        this.authorName = authorName;
+        this.publishingYear = publishingYear;
+    }
+}
